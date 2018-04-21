7bec486101d9e59cba7c49c357c78629e44f5559
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-04-21 18:03:49 -0300

    :art: Improve urls file

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,4 @@ from django.conf.urls import patterns, url
 
 from app.views import index
 
-urlpatterns = patterns('',
-                       url(r'^$', index, name='index'),
-                       )
+urlpatterns = patterns('', url(r'^$', index, name='index'))
