7e2782697a881b0dffcb2576a358320d45b89f98
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-04-21 22:58:00 -0300

    :ambulance: Fix WSGI

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,11 +1,7 @@
 import os
-from sys import path
 
 from django.core.wsgi import get_wsgi_application
 
-SITE_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-path.append(SITE_ROOT)
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.production")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
