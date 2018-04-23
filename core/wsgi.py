996dca4720b9d14b650f101a4b477ffe055a644d
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-04-23 00:30:22 -0300

    :fire: Stop running "collectstatic" automatically

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,13 +1,7 @@
 import os
 
-from django.conf import settings
-from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
-
-
-if not settings.TEST:  # pragma: no cover
-    call_command('collectstatic', interactive=False)
